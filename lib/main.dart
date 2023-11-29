c74fa63d4958e407e79c2deddb7e09719674f2a0
DanielMartinsSouza <danielmartsouza@gmail.com>
2023-11-29 19:05:19 -0300

    Inicio da estilização dos componentes da lista

--- lib/main.dart
+++ lib/main.dart
@@ -57,12 +57,40 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Container(
-                      child: Text(transactionMap.value.toString()),
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        transactionMap.value.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(transactionMap.title),
-                        Text(transactionMap.date.toString())
+                        Text(
+                          transactionMap.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          transactionMap.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        )
                       ],
                     ),
                   ],
